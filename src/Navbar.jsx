0481ff39a21977e5e4ae80c5f83ec73f0932c045
varshith9346 <narishettivarshith@gmail.com>
2025-03-25 16:01:06 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
     <>
  <nav  className="navbar navbar-expand-lg bg-body-tertiary">
   <div  className="container-fluid">
-    <a  className="navbar-brand" href="#">SUPERBAZAR</a>
+    <a  className="navbar-brand" href="#">FashionMug</a>
     <button  className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span  className="navbar-toggler-icon"></span>
     </button>
